a7527489c5019bd498e5339ba207d9bb3fa0d1ab
liucan80 <190633933@qq.com>
2017-12-05 16:29:09 +0800

    1.基本完成预想功能 2.函数编程避免使用Goto 3.python无do-while可以用while+if+break替代 4.selenium可以判断页面元素的一些基本状态

--- login-taobao.py
+++ login-taobao.py
@@ -7,6 +7,109 @@ import sqlite3
 import re
 import platform
 import traceback
+
+def findandstore(pagenumber,ID):
+
+    if pagenumber==1:
+        driver.implicitly_wait(10)
+        boughttables = driver.find_elements_by_class_name("bought-wrapper-mod__table___3xFFM")
+        for table in boughttables:
+            ID = ID + 1
+            # print(table)
+            print(table.text)
+            # OrderNumber=re.findall(r"\d{4}-\d{2}-\d{2}",table.text)
+            DateOfOrder = table.find_element_by_class_name("bought-wrapper-mod__checkbox-label___3Va60").text
+            print(DateOfOrder)
+            OrderNumber = "".join(re.findall(r"\d{15,18}", table.text))
+            print(OrderNumber)
+            ParentOrder = OrderNumber
+            NameOfShop = table.find_element_by_class_name("bought-wrapper-mod__seller-container___3dAK3").text
+            print(NameOfShop)
+            LinkOfShop = table.find_element_by_tag_name("a").get_attribute('href')
+            print(LinkOfShop)
+            c.execute("INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','','','','','','','','')" % (
+            ID, DateOfOrder, OrderNumber, NameOfShop, LinkOfShop))
+
+            ProductionListsBody = table.find_elements_by_tag_name("tbody")
+            del ProductionListsBody[0]
+            for ProductionLists in ProductionListsBody:
+                trs = ProductionLists.find_elements_by_tag_name("tr")
+                for Productionlist in trs:
+                    Colms = Productionlist.find_elements_by_tag_name("td")
+                    Production = Colms[0].find_element_by_class_name("suborder-mod__production___3WebF").text
+                    LinkOfProduction = Colms[0].find_element_by_tag_name("a").get_attribute('href')
+                    if Production == '保险服务':
+                        break
+                    UnitPrice = Colms[1].text
+                    Quantity = Colms[2].text
+                    ActualCost = Colms[4].text
+                    StatusOfTrade = Colms[5].text
+                    ID = ID + 1
+                    c.execute(
+                        "INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                            ID, DateOfOrder, OrderNumber, NameOfShop, LinkOfShop, ParentOrder, 'test', Production,
+                            LinkOfProduction,
+                            UnitPrice, Quantity, ActualCost, StatusOfTrade))
+                    #     SubOrders=table.find_elements_by_class_name("suborder-mod__production___3WebF")
+        conn.commit()
+        return ID
+    elif pagenumber>1:
+        driver.implicitly_wait(10)
+        boughttables = driver.find_elements_by_class_name("bought-wrapper-mod__table___3xFFM")
+        for table in boughttables:
+            ID = ID + 1
+            # print(table)
+            print(table.text)
+            # OrderNumber=re.findall(r"\d{4}-\d{2}-\d{2}",table.text)
+            DateOfOrder = table.find_element_by_class_name("bought-wrapper-mod__checkbox-label___3Va60").text
+            print(DateOfOrder)
+            OrderNumber = "".join(re.findall(r"\d{15,18}", table.text))
+            print(OrderNumber)
+            ParentOrder = OrderNumber
+            NameOfShop = table.find_element_by_class_name("bought-wrapper-mod__seller-container___3dAK3").text
+            print(NameOfShop)
+            LinkOfShop = table.find_element_by_tag_name("a").get_attribute('href')
+            print(LinkOfShop)
+            c.execute("INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','','','','','','','','')" % (
+                ID, DateOfOrder, OrderNumber, NameOfShop, LinkOfShop))
+
+            ProductionListsBody = table.find_elements_by_tag_name("tbody")
+            del ProductionListsBody[0]
+            for ProductionLists in ProductionListsBody:
+                trs = ProductionLists.find_elements_by_tag_name("tr")
+                for Productionlist in trs:
+                    Colms = Productionlist.find_elements_by_tag_name("td")
+                    try:
+                        Production = Colms[0].find_element_by_class_name("suborder-mod__production___3WebF").text
+                        LinkOfProduction = Colms[0].find_element_by_tag_name("a").get_attribute('href')
+                    except:
+                        Production=Colms[0].text
+                        LinkOfProduction = ''
+                    if LinkOfProduction == "保险服务":
+                        break
+                    UnitPrice = Colms[1].text
+                    Quantity = Colms[2].text
+                    ActualCost=Colms[4].text
+                    # try:
+                    #     ActualCost = Colms[4].find_element_by_class_name("price-mod__withIcon___z4CVt").text
+                    # except:
+                    #     ActualCost = Colms[4].text
+                    # finally:
+                    #     print("")
+                    StatusOfTrade = Colms[5].text
+                    ID = ID + 1
+                    c.execute(
+                        "INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                            ID, DateOfOrder, OrderNumber, NameOfShop, LinkOfShop, ParentOrder, 'test', Production,
+                            LinkOfProduction,
+                            UnitPrice, Quantity, ActualCost, StatusOfTrade))
+                    #     SubOrders=table.find_elements_by_class_name("suborder-mod__production___3WebF")
+        conn.commit()
+        return ID
+    else:
+        print("完成")
+
+        return ID
 if platform.system()=="Windows":
     driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
     conn=sqlite3.connect("c:/test.db")
@@ -56,6 +159,7 @@ except:
                   );''')
 finally:
     print("成功创建数据表")
+
 try:
     print("请扫描二维码")
     element = WebDriverWait(driver, 20).until(
@@ -67,50 +171,50 @@ try:
     # driver.implicitly_wait(5)
     driver.find_element_by_id("bought").click()
     driver.implicitly_wait(10)
-    boughttables = driver.find_elements_by_class_name("bought-wrapper-mod__table___3xFFM")
-    #print(boughttables)
-    i = 0
-    for table in boughttables:
-        i = i + 1
-        #print(table)
-        print(table.text)
-        #OrderNumber=re.findall(r"\d{4}-\d{2}-\d{2}",table.text)
-        DateOfOrder = table.find_element_by_class_name("bought-wrapper-mod__checkbox-label___3Va60").text
-        print(DateOfOrder)
-        OrderNumber = "".join(re.findall(r"\d{16,18}", table.text))
-        print(OrderNumber)
-        ParentOrder=OrderNumber
-        NameOfShop = table.find_element_by_class_name("bought-wrapper-mod__seller-container___3dAK3").text
-        print(NameOfShop)
-        LinkOfShop = table.find_element_by_tag_name("a").get_attribute('href')
-        print(LinkOfShop)
-        c.execute("INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','','','','','','','','')" % (i, DateOfOrder, OrderNumber,NameOfShop,LinkOfShop))
-
-        ProductionListsBody=table.find_elements_by_tag_name("tbody")
-        del ProductionListsBody[0]
-        for ProductionLists in ProductionListsBody:
-            trs=ProductionLists.find_elements_by_tag_name("tr")
-            for Productionlist in trs:
-                Colms=Productionlist.find_elements_by_tag_name("td")
-                Production=Colms[0].find_element_by_class_name("suborder-mod__production___3WebF").text
-                LinkOfProduction=Colms[0].find_element_by_tag_name("a").get_attribute('href')
-                if LinkOfProduction=="保险服务":
-                    break
-                UnitPrice=Colms[1].text
-                Quantity=Colms[2].text
-                try:
-                    ActualCost=Colms[4].find_element_by_class_name("price-mod__withIcon___z4CVt").text
-                except:
-                    ActualCost=Colms[4].text
-                finally:
-                    print("")
-                StatusOfTrade=Colms[5].text
-                i=i+1
-                c.execute(
-                    "INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
-                    i, DateOfOrder, OrderNumber, NameOfShop, LinkOfShop, ParentOrder,'test', Production, LinkOfProduction,
-                    UnitPrice,Quantity,ActualCost,StatusOfTrade))
-                #     SubOrders=table.find_elements_by_class_name("suborder-mod__production___3WebF")
+    # boughttables = driver.find_elements_by_class_name("bought-wrapper-mod__table___3xFFM")
+    # #print(boughttables)
+    # i = 0
+    # for table in boughttables:
+    #     i = i + 1
+    #     #print(table)
+    #     print(table.text)
+    #     #OrderNumber=re.findall(r"\d{4}-\d{2}-\d{2}",table.text)
+    #     DateOfOrder = table.find_element_by_class_name("bought-wrapper-mod__checkbox-label___3Va60").text
+    #     print(DateOfOrder)
+    #     OrderNumber = "".join(re.findall(r"\d{16,18}", table.text))
+    #     print(OrderNumber)
+    #     ParentOrder=OrderNumber
+    #     NameOfShop = table.find_element_by_class_name("bought-wrapper-mod__seller-container___3dAK3").text
+    #     print(NameOfShop)
+    #     LinkOfShop = table.find_element_by_tag_name("a").get_attribute('href')
+    #     print(LinkOfShop)
+    #     c.execute("INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','','','','','','','','')" % (i, DateOfOrder, OrderNumber,NameOfShop,LinkOfShop))
+    #
+    #     ProductionListsBody=table.find_elements_by_tag_name("tbody")
+    #     del ProductionListsBody[0]
+    #     for ProductionLists in ProductionListsBody:
+    #         trs=ProductionLists.find_elements_by_tag_name("tr")
+    #         for Productionlist in trs:
+    #             Colms=Productionlist.find_elements_by_tag_name("td")
+    #             Production=Colms[0].find_element_by_class_name("suborder-mod__production___3WebF").text
+    #             LinkOfProduction=Colms[0].find_element_by_tag_name("a").get_attribute('href')
+    #             if LinkOfProduction=="保险服务":
+    #                 break
+    #             UnitPrice=Colms[1].text
+    #             Quantity=Colms[2].text
+    #             try:
+    #                 ActualCost=Colms[4].find_element_by_class_name("price-mod__withIcon___z4CVt").text
+    #             except:
+    #                 ActualCost=Colms[4].text
+    #             finally:
+    #                 print("")
+    #             StatusOfTrade=Colms[5].text
+    #             i=i+1
+    #             c.execute(
+    #                 "INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+    #                 i, DateOfOrder, OrderNumber, NameOfShop, LinkOfShop, ParentOrder,'test', Production, LinkOfProduction,
+    #                 UnitPrice,Quantity,ActualCost,StatusOfTrade))
+    #             #     SubOrders=table.find_elements_by_class_name("suborder-mod__production___3WebF")
     #     print(SubOrders)
     #     UnitPrices=table.find_elements_by_class_name("price-mod__price___3Z88i")
     #     print(UnitPrices)
@@ -125,7 +229,24 @@ try:
     #             break
     #         i = i + 1
     #         c.execute("INSERT INTO boughtlist VALUES(%d,'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','')" % (i, DateOfOrder, OrderNumber,NameOfShop,LinkOfShop,ParentOrder,Production,LinkOfProduction,'test',UnitPrice,'test','test','test'))
-    conn.commit()
+    # conn.commit()
+    pagenumber=1
+    ID=0
+    contents = driver.find_element_by_class_name("simple-pagination-mod__container___2bG5u").find_elements_by_tag_name(
+        "button")
+    while True:
+        if pagenumber>=1:
+            ID=findandstore(pagenumber, ID)
+            contents[1].click()
+            time.sleep(6)
+            pagenumber=pagenumber+1
+            if not contents[1].is_enabled():
+                ID = findandstore(pagenumber, ID)
+                break
+
+
+
+
     conn.close()
 except Exception as e:
    # c.execute('''DROP TABLE boughtlist;''')
@@ -135,3 +256,4 @@ except Exception as e:
     print("未扫描二维码")
 finally:
     print("done")
+
